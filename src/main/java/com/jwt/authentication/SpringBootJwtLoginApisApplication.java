7c236d24c9a43525033ac3225ecc32e01a4d25f0
amarjeet <amarjeet2kr@gmail.com>
2024-01-02 22:53:53 +0530

    Initial version

---
+++ src/main/java/com/jwt/authentication/SpringBootJwtLoginApisApplication.java
@@ -0,0 +1,13 @@
+package com.jwt.authentication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJwtLoginApisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootJwtLoginApisApplication.class, args);
+	}
+
+}
